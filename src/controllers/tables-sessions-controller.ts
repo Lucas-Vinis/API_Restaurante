d8b67d4c0f9960947b9c23ac90633ed072afcd40
Lucas <luksvinis@gmail.com>
2026-09-23 20:07:51 -0300

    Criando as mesas e a relação de abertura e fechamento de mesas, com as rotas e controllers

---
+++ src/controllers/tables-sessions-controller.ts
@@ -0,0 +1,63 @@
+import { NextFunction, Request, Response } from "express"
+import { knex } from "@/database/knex"
+import { z } from "zod"
+import { AppError } from "@/utils/AppError"
+
+class TablesSessionsController {
+    async create(request: Request, response: Response, next: NextFunction){
+        try {
+            const bodySchema = z.object({table_id: z.number()})
+
+            const { table_id } = bodySchema.parse(request.body)
+
+            const session = await knex<TableSessionsRepository>("tables_sessions").where({table_id}).orderBy("opened_at", "desc").first()
+
+            if(session && !session.closed_at){
+                throw new AppError("this table is already open")
+            }
+
+            await knex<TableSessionsRepository>("tables_sessions").insert({
+                table_id,
+                opened_at: knex.fn.now()
+            })
+
+            return response.json()
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async index(request: Request, response: Response, next: NextFunction){
+        try {
+            const session = await knex<TableSessionsRepository>("tables_sessions").orderBy("closed_at")
+
+            return response.json(session)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async update(request: Request, response: Response, next: NextFunction){
+        try {
+            const id = z.string().transform((value) => Number(value)).refine((value) => !isNaN(value), {message: "id must be a number!"}).parse(request.params.id)
+
+            const session = await knex<TableSessionsRepository>("tables_sessions").where({id}).first()
+
+            if(!session){
+                throw new AppError("this table weren't opened")
+            }
+
+            if(session.closed_at){
+                throw new AppError("this table is already closed")
+            }
+
+            await knex<TableSessionsRepository>("tables_sessions").update({closed_at: knex.fn.now()}).where({id})
+
+            return response.json()
+        } catch (error) {
+            next(error)
+        }
+    }
+}
+
+export { TablesSessionsController }
